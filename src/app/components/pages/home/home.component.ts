9c5d296ffa22dfb90e94c14f064258e1470b3ab4
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-05-18 20:09:23 -0300

    style: home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import { DatePipe } from '@angular/common';
 import { environment } from '../../../../environment/environment';
 import { MomemtsService } from '../../../services/momemts.service';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Moment } from '../../../Moment';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgFor],
+  imports: [
+    NgFor,
+    NgIf,
+    RouterLink,
+    DatePipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
