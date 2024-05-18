35946f345573b75cd778311b0432269e3b91f817
Isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-05-18 19:33:54 -0300

    feat: getting all data of the api

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 
+import { environment } from '../../../../environment/environment';
+import { MomemtsService } from '../../../services/momemts.service';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Moment } from '../../../Moment';
+import { NgFor } from '@angular/common';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  public allMoments: Moment[] = [];
+  public moments: Moment[] = []
+  public baseApiUrl = environment.baseApiUrl;
+
+  constructor(private momentService: MomemtsService) {}
+
+  ngOnInit() {
+    this.momentService.getMoments().subscribe((itens) => {
+
+      const data = itens.data;
+
+      data.map((item) => {
+        item.create_at = new Date(item.create_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+
+    });
+  }
 }
